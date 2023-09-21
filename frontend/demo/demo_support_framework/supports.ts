360d8eb093f31515dedda0d6c79bbc7ee77ef4cb
lfgrc <limingyuaegon@gmail.com>
2023-09-21 05:37:02 +0000

    modify demo photos

--- frontend/demo/demo_support_framework/supports.ts
+++ frontend/demo/demo_support_framework/supports.ts
@@ -25,13 +25,13 @@ export const demoImages: TaggedImage[] = [
 		"body": {
 			"id": 9,
 			"device_id": 8,
-			"attachment_processed_at": "2017-06-01T14:16:55.709Z",
-			"updated_at": "2017-06-01T14:16:55.715Z",
-			"created_at": "2017-06-01T14:15:50.666Z",
+			"attachment_processed_at": "2017-06-03T14:16:55.709Z",
+			"updated_at": "2017-06-03T14:16:55.715Z",
+			"created_at": "2017-06-03T14:15:50.666Z",
 			"attachment_url": "https://www.realsimple.com/thmb/P9g1f-xU0Zr2cq2_3dMwfXizZcM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/impossible-to-kill-outdoor-plants-1-2000-f513b0574cb04674a1bce40b832b28dd.jpg",
 			"meta": {
-				"x": 632,
-				"y": 347,
+				"x": 300,
+				"y": 300,
 				"z": 164
 			}
 		},
@@ -43,13 +43,13 @@ export const demoImages: TaggedImage[] = [
 		"body": {
 			"id": 8,
 			"device_id": 8,
-			"attachment_processed_at": "2017-06-01T14:16:45.899Z",
-			"updated_at": "2017-06-01T14:16:45.903Z",
-			"created_at": "2017-06-01T14:14:22.747Z",
+			"attachment_processed_at": "2017-06-02T14:16:45.899Z",
+			"updated_at": "2017-06-02T14:16:45.903Z",
+			"created_at": "2017-06-02T14:14:22.747Z",
 			"attachment_url": "https://images.immediate.co.uk/production/volatile/sites/10/2021/04/2048x1365-Strelitzia-reginae-GettyImages-1270647929-4f76714.jpg?quality=90&resize=940,627",
 			"meta": {
-				"x": 632,
-				"y": 347,
+				"x": 1000,
+				"y": 300,
 				"z": 164
 			}
 		},
@@ -66,8 +66,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-06-01T14:14:22.726Z",
 			"attachment_url": "https://www.realsimple.com/thmb/1mcrAQoWk3QQgdjYr9hWErYOm_U=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/trends-real-simple-Home-2023-green-e1314b42c49b43449a79e95634b0fbca.jpg",
 			"meta": {
-				"x": 266,
-				"y": 330,
+				"x": 300,
+				"y": 800,
 				"z": 53
 			}
 		},
