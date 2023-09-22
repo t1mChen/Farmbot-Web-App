d835029619c961c4e3e8d58a851c1e98e749c03b
Reesedog <hghghghg110@126.com>
2023-09-22 04:55:57 +0000

    image update

--- frontend/demo/demo_support_framework/supports.ts
+++ frontend/demo/demo_support_framework/supports.ts
@@ -30,8 +30,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-06-03T14:15:50.666Z",
 			"attachment_url": "https://i.imgur.com/LLajqT3.jpeg",
 			"meta": {
-				"x": 150,
-				"y": 100,
+				"x": 150+20,
+				"y": 100+20,
 				"z": 164
 			}
 		},
@@ -48,8 +48,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-06-02T14:14:22.747Z",
 			"attachment_url": "https://i.imgur.com/jgZMupJ.jpeg",
 			"meta": {
-				"x": 150,
-				"y": 500,
+				"x": 950+20,
+				"y": 900+20,
 				"z": 164
 			}
 		},
@@ -66,8 +66,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-06-01T14:14:22.726Z",
 			"attachment_url": "https://i.imgur.com/WsqcMr3.jpeg",
 			"meta": {
-				"x": 150,
-				"y": 900,
+				"x": 150+20,
+				"y": 900+20,
 				"z": 53
 			}
 		},
@@ -84,8 +84,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-05-28T14:15:50.666Z",
 			"attachment_url": "https://i.imgur.com/EudVDLU.jpeg",
 			"meta": {
-				"x": 550,
-				"y": 100,
+				"x": 550+20,
+				"y": 100+20,
 				"z": 164
 			}
 		},
@@ -102,8 +102,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-05-27T14:15:50.666Z",
 			"attachment_url": "https://i.imgur.com/Xou4Ubz.jpeg",
 			"meta": {
-				"x": 550,
-				"y": 500,
+				"x": 150+20,
+				"y": 500+20,
 				"z": 164
 			}
 		},
@@ -120,8 +120,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-05-26T14:15:50.666Z",
 			"attachment_url": "https://i.imgur.com/kAmrAZy.jpeg",
 			"meta": {
-				"x": 550,
-				"y": 900,
+				"x": 550+20,
+				"y": 900+20,
 				"z": 164
 			}
 		},
@@ -138,8 +138,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-05-25T14:15:50.666Z",
 			"attachment_url": "https://i.imgur.com/emxArQX.jpeg",
 			"meta": {
-				"x": 950,
-				"y": 100,
+				"x": 950+20,
+				"y": 100+20,
 				"z": 164
 			}
 		},
@@ -156,8 +156,8 @@ export const demoImages: TaggedImage[] = [
 			"created_at": "2017-05-21T14:15:50.666Z",
 			"attachment_url": "https://i.imgur.com/U3mBYpG.jpeg",
 			"meta": {
-				"x": 950,
-				"y": 500,
+				"x": 950+20,
+				"y": 500+20,
 				"z": 164
 			}
 		},
@@ -172,10 +172,10 @@ export const demoImages: TaggedImage[] = [
 			"attachment_processed_at": "2017-05-20T14:16:55.709Z",
 			"updated_at": "2017-05-20T14:16:55.715Z",
 			"created_at": "2017-05-20T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/EVo6XOU.jpeg",
+			"attachment_url": "https://i.imgur.com/K3ye6hH.jpeg",
 			"meta": {
-				"x": 950,
-				"y": 900,
+				"x": 550+20,
+				"y": 500+20,
 				"z": 164
 			}
 		},
@@ -183,59 +183,4 @@ export const demoImages: TaggedImage[] = [
 	},
 ];
 
-export const flipperImages: TaggedImage[] = [
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 7,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-25T14:16:55.709Z",
-			"updated_at": "2017-05-25T14:16:55.715Z",
-			"created_at": "2017-05-25T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/emxArQX.jpeg",
-			"meta": {
-				"x": 950,
-				"y": 100,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.6"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 8,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-21T14:16:55.709Z",
-			"updated_at": "2017-05-21T14:16:55.715Z",
-			"created_at": "2017-05-21T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/U3mBYpG.jpeg",
-			"meta": {
-				"x": 950,
-				"y": 500,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.5"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 9,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-20T14:16:55.709Z",
-			"updated_at": "2017-05-20T14:16:55.715Z",
-			"created_at": "2017-05-20T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/EVo6XOU.jpeg",
-			"meta": {
-				"x": 950,
-				"y": 900,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.4"
-	},
-]
+export const flipperImages: TaggedImage[] = demoImages;
