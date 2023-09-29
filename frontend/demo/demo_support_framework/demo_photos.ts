91683de1923bccbfbd8c290af77ac04df44c8e14
lfgrc <limingyuaegon@gmail.com>
2023-09-29 06:23:03 +0000

    Add comparing logic to photo flipper.

---
+++ frontend/demo/demo_support_framework/demo_photos.ts
@@ -0,0 +1,188 @@
+import {
+	SpecialStatus,
+	TaggedImage,
+} from "farmbot";
+
+// a sample of images for demo
+export const demoPhotos: TaggedImage[] = [
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 1,
+			"device_id": 8,
+			"attachment_processed_at": "2017-06-03T14:16:55.709Z",
+			"updated_at": "2017-06-03T14:16:55.715Z",
+			"created_at": "2017-06-03T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/LLajqT3.jpeg",
+			"meta": {
+				"x": 200,
+				"y": 200,
+				"z": 164
+			}
+		},
+		"uuid": "Image.9.3"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 10,
+			"device_id": 8,
+			"attachment_processed_at": "2017-06-03T14:16:54.709Z",
+			"updated_at": "2017-06-03T14:16:54.715Z",
+			"created_at": "2017-06-03T14:15:49.666Z",
+			"attachment_url": "https://i.imgur.com/jgZMupJ.jpeg",
+			"meta": {
+				"x": 200,
+				"y": 200,
+				"z": 164
+			}
+		},
+		"uuid": "Image.9.2"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 2,
+			"device_id": 8,
+			"attachment_processed_at": "2017-06-02T14:16:45.899Z",
+			"updated_at": "2017-06-02T14:16:45.903Z",
+			"created_at": "2017-06-02T14:14:22.747Z",
+			"attachment_url": "https://i.imgur.com/EVo6XOU.jpeg",
+			"meta": {
+				"x": 200,
+				"y": 600,
+				"z": 164
+			}
+		},
+		"uuid": "Image.8.4"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 3,
+			"device_id": 8,
+			"attachment_processed_at": "2017-06-01T14:16:34.839Z",
+			"updated_at": "2017-06-01T14:16:34.984Z",
+			"created_at": "2017-06-01T14:14:22.726Z",
+			"attachment_url": "https://i.imgur.com/WsqcMr3.jpeg",
+			"meta": {
+				"x": 200,
+				"y": 1000,
+				"z": 53
+			}
+		},
+		"uuid": "Image.7.5"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 4,
+			"device_id": 8,
+			"attachment_processed_at": "2017-05-28T14:16:55.709Z",
+			"updated_at": "2017-05-28T14:16:55.715Z",
+			"created_at": "2017-05-28T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/EudVDLU.jpeg",
+			"meta": {
+				"x": 600,
+				"y": 200,
+				"z": 164
+			}
+		},
+		"uuid": "Image.6.9"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 5,
+			"device_id": 8,
+			"attachment_processed_at": "2017-05-27T14:16:55.709Z",
+			"updated_at": "2017-05-27T14:16:55.715Z",
+			"created_at": "2017-05-27T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/Xou4Ubz.jpeg",
+			"meta": {
+				"x": 600,
+				"y": 600,
+				"z": 164
+			}
+		},
+		"uuid": "Image.6.8"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 6,
+			"device_id": 8,
+			"attachment_processed_at": "2017-05-26T14:16:55.709Z",
+			"updated_at": "2017-05-26T14:16:55.715Z",
+			"created_at": "2017-05-26T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/kAmrAZy.jpeg",
+			"meta": {
+				"x": 600,
+				"y": 1000,
+				"z": 164
+			}
+		},
+		"uuid": "Image.6.7"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 7,
+			"device_id": 8,
+			"attachment_processed_at": "2017-05-25T14:16:55.709Z",
+			"updated_at": "2017-05-25T14:16:55.715Z",
+			"created_at": "2017-05-25T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/emxArQX.jpeg",
+			"meta": {
+				"x": 1000,
+				"y": 200,
+				"z": 164
+			}
+		},
+		"uuid": "Image.6.6"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 8,
+			"device_id": 8,
+			"attachment_processed_at": "2017-05-21T14:16:55.709Z",
+			"updated_at": "2017-05-21T14:16:55.715Z",
+			"created_at": "2017-05-21T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/U3mBYpG.jpeg",
+			"meta": {
+				"x": 1000,
+				"y": 600,
+				"z": 164
+			}
+		},
+		"uuid": "Image.6.5"
+	},
+	{
+		"kind": "Image",
+		"specialStatus": SpecialStatus.SAVED,
+		"body": {
+			"id": 9,
+			"device_id": 8,
+			"attachment_processed_at": "2017-05-20T14:16:55.709Z",
+			"updated_at": "2017-05-20T14:16:55.715Z",
+			"created_at": "2017-05-20T14:15:50.666Z",
+			"attachment_url": "https://i.imgur.com/jgZMupJ.jpeg",
+			"meta": {
+				"x": 1000,
+				"y": 1000,
+				"z": 164
+			}
+		},
+		"uuid": "Image.6.4"
+	},
+];
--- frontend/demo/demo_support_framework/supports.ts
+++ frontend/demo/demo_support_framework/supports.ts
@@ -8,6 +8,7 @@ import {
 	Xyz,
 } from "farmbot";
 import cloneDeep from 'lodash/cloneDeep';
+import { demoPhotos } from "./demo_photos";
 
 // a sample webcam feed for demo
 export const demoWebcamFeed: TaggedWebcamFeed = {
@@ -34,7 +35,11 @@ export const map_limit = {
 	z: 400,
 };
 
-export const demoImages: TaggedImage[] = [
+// a sample of images for demo
+export var demoImages: TaggedImage[] = demoPhotos;
+
+// place holder for compare Images
+export var compareImages: TaggedImage[] = [
 	{
 		"kind": "Image",
 		"specialStatus": SpecialStatus.SAVED,
@@ -57,147 +62,24 @@ export const demoImages: TaggedImage[] = [
 		"kind": "Image",
 		"specialStatus": SpecialStatus.SAVED,
 		"body": {
-			"id": 2,
+			"id": 10,
 			"device_id": 8,
-			"attachment_processed_at": "2017-06-02T14:16:45.899Z",
-			"updated_at": "2017-06-02T14:16:45.903Z",
-			"created_at": "2017-06-02T14:14:22.747Z",
-			"attachment_url": "https://i.imgur.com/EVo6XOU.jpeg",
-			"meta": {
-				"x": 200,
-				"y": 600,
-				"z": 164
-			}
-		},
-		"uuid": "Image.8.4"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 3,
-			"device_id": 8,
-			"attachment_processed_at": "2017-06-01T14:16:34.839Z",
-			"updated_at": "2017-06-01T14:16:34.984Z",
-			"created_at": "2017-06-01T14:14:22.726Z",
-			"attachment_url": "https://i.imgur.com/WsqcMr3.jpeg",
-			"meta": {
-				"x": 200,
-				"y": 1000,
-				"z": 53
-			}
-		},
-		"uuid": "Image.7.5"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 4,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-28T14:16:55.709Z",
-			"updated_at": "2017-05-28T14:16:55.715Z",
-			"created_at": "2017-05-28T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/EudVDLU.jpeg",
-			"meta": {
-				"x": 600,
-				"y": 200,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.9"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 5,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-27T14:16:55.709Z",
-			"updated_at": "2017-05-27T14:16:55.715Z",
-			"created_at": "2017-05-27T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/Xou4Ubz.jpeg",
-			"meta": {
-				"x": 600,
-				"y": 600,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.8"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 6,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-26T14:16:55.709Z",
-			"updated_at": "2017-05-26T14:16:55.715Z",
-			"created_at": "2017-05-26T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/kAmrAZy.jpeg",
-			"meta": {
-				"x": 600,
-				"y": 1000,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.7"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 7,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-25T14:16:55.709Z",
-			"updated_at": "2017-05-25T14:16:55.715Z",
-			"created_at": "2017-05-25T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/emxArQX.jpeg",
-			"meta": {
-				"x": 1000,
-				"y": 200,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.6"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 8,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-21T14:16:55.709Z",
-			"updated_at": "2017-05-21T14:16:55.715Z",
-			"created_at": "2017-05-21T14:15:50.666Z",
-			"attachment_url": "https://i.imgur.com/U3mBYpG.jpeg",
-			"meta": {
-				"x": 1000,
-				"y": 600,
-				"z": 164
-			}
-		},
-		"uuid": "Image.6.5"
-	},
-	{
-		"kind": "Image",
-		"specialStatus": SpecialStatus.SAVED,
-		"body": {
-			"id": 9,
-			"device_id": 8,
-			"attachment_processed_at": "2017-05-20T14:16:55.709Z",
-			"updated_at": "2017-05-20T14:16:55.715Z",
-			"created_at": "2017-05-20T14:15:50.666Z",
+			"attachment_processed_at": "2017-06-03T14:16:54.709Z",
+			"updated_at": "2017-06-03T14:16:54.715Z",
+			"created_at": "2017-06-03T14:15:49.666Z",
 			"attachment_url": "https://i.imgur.com/jgZMupJ.jpeg",
 			"meta": {
-				"x": 1000,
-				"y": 1000,
+				"x": 200,
+				"y": 200,
 				"z": 164
 			}
 		},
-		"uuid": "Image.6.4"
+		"uuid": "Image.9.2"
 	},
-];
+]
+
+// flag to check if comparing photos
+export var isComparing: boolean = false
 
 export var demoCurrentImage: TaggedImage | undefined = demoImages[0];
 export function setCurrentImage(image: TaggedImage) {
